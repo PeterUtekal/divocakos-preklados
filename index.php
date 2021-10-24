f3426c41d102377083b2c1c596fb02af0971e16b
Peter Utekal <popertolps@gmail.com>
2021-10-24 20:56:05 +0200

    github link

--- index.php
+++ index.php
@@ -6,24 +6,24 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
-<style>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+		<style>
 
-@media(max-width:960px){
-	form{
-		flex-wrap:wrap;
-	}
-}
-</style>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JQREL3RSYJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+		@media(max-width:960px){
+			form{
+				flex-wrap:wrap;
+			}
+		}
+		</style>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-JQREL3RSYJ"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
 
-  gtag('config', 'G-JQREL3RSYJ');
-</script>
+		  gtag('config', 'G-JQREL3RSYJ');
+		</script>
 
 		</head>
 		<body>
@@ -45,6 +45,11 @@
 						<li class="nav-item">
 						  <a class="nav-link" href="https://www.facebook.com/profile.php?id=100014015532696"><i class='bi bi-facebook'></i></a>
 						</li>
+						
+						<li class="nav-item">
+						  <a class="nav-link" href="https://github.com/PeterUtekal/divocakos-preklados"><i class='bi bi-github'></i></a>
+						</li>
+						
 					  </ul>
 					</div>
 				  </div>
